7250c090190d9d10be1669e79e99f4f66b0d6c98
Yuan Vei Shen <vyua0001@student.monash.edu>
2021-05-22 14:21:59 +0800

    Tutor Refactoring ongoing

--- Assignment - copy/src/tutorview/TutorAllContracts.java
+++ Assignment - copy/src/tutorview/TutorAllContracts.java
@@ -1,76 +1,133 @@
 package tutorview;
-import java.awt.BorderLayout;
+import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import javax.swing.*;
 
-import mainview.Display;
-import mainview.ListPanel;
-import mainview.MouseClickListener;
-import mainview.Utils;
+import mainview.*;
 import model.Contract;
 import model.User;
 
 /**
  * This is the View for the Tutor to see all the contracts that involves them as the second party
  */
-public class TutorAllContracts extends TutorView {
-	public TutorAllContracts(Display display, User user) {
-		super(display, user);
+public class TutorAllContracts extends JPanel implements Observer {
+//	public static final int CONTRACT_QUOTA = 5;
+	private JList<Contract> contractList;
+	private Contract subscriber;
+	List<Contract> contracts;
+	private User user;
+
+	public TutorAllContracts(User user, Contract subsriber) {
+		super(new BorderLayout());
+		this.user = user;
+		this.subscriber = subsriber;
+		this.contracts = subscriber.getAllContractsAsFirstParty(user.getId());
+		placeComponents();
 	}
 
-	@Override
-	protected void placeComponents() {
-		super.placeComponents();
-		
-		List<Contract> contracts = new ArrayList<Contract>();
-		contracts = Contract.getAllContractsAsSecondParty(user.getId());
-		
-		List<JComponent> comp = new ArrayList<JComponent>();
-		
+	private void placeComponents() {
+		DefaultListModel<Contract> model = new DefaultListModel<Contract>();
+		for (Contract c : contracts)
+			model.addElement(c);
+		contractList = new JList<>(model);
+
+		contractList.setCellRenderer(new ContractCellRenderer());
+
+		JScrollPane scrollp = new JScrollPane(contractList);
+		this.add(scrollp);
+//		List<Contract> contracts = new ArrayList<Contract>();
+//		contracts = Contract.getAllContractsAsSecondParty(user.getId());
+//
+//		List<JComponent> comp = new ArrayList<JComponent>();
+//
+//		for (Contract c : contracts) {
+//			JPanel panel = new JPanel();
+//			if (c.secondPartySigned()) {
+//				JTextArea tA = new JTextArea();
+//				tA.setText(c.toString());
+//				tA.setEditable(false);
+//				panel.add(tA);
+//			} else if (c.secondPartySigned() == false) {
+//				JTextArea tA = new JTextArea();
+//				JButton bT = new JButton("Sign");
+//
+//				tA.setText(c.toString());
+//				tA.setEditable(false);
+//				panel.add(tA, BorderLayout.WEST);
+//				panel.add(bT, BorderLayout.EAST);
+//
+//				bT.addMouseListener(new MouseClickListener() {
+//					@Override
+//					public void mouseClicked(MouseEvent e) {
+//						c.secondPartySign(true);
+//						if (c.isSigned()) {
+//							c.signContract();
+//							Utils.CONTRACT_SIGNED.show();
+//						} else
+//							Utils.OTHER_PARTY_PENDING.show();
+//					}
+//				});
+//			}
+//			comp.add(panel);
+//		}
+//
+//		JPanel midPanel = new ListPanel(comp);
+//        main.add(midPanel);
+//		JScrollPane scrollp = new JScrollPane(midPanel);
+//		main.add(scrollp);
+//		this.display.setVisible();
+	}
+
+	public void setSignContractListener(MouseClickListener listener) {
+		contractList.addMouseListener(listener);
+	}
+
+	public Contract getSelectedContract() {
+		return contractList.getSelectedValue();
+	}
+
+	public int getSignedContracts() {
+		int cnt = 0;
 		for (Contract c : contracts) {
+			if (c.isSigned())
+				cnt++;
+		}
+		return cnt;
+	}
+
+	private class ContractCellRenderer extends JPanel implements ListCellRenderer<Contract> {
+
+		@Override
+		public Component getListCellRendererComponent(JList<? extends Contract> list, Contract c, int index,
+													  boolean isSelected, boolean cellHasFocus) {
+			this.removeAll();
 			JPanel panel = new JPanel();
-			if (c.secondPartySigned()) {
+			if (c.firstPartySigned()) {
 				JTextArea tA = new JTextArea();
 				tA.setText(c.toString());
 				tA.setEditable(false);
 				panel.add(tA);
-			} else if (c.secondPartySigned() == false) {
+			} else if (c.firstPartySigned() == false) {
 				JTextArea tA = new JTextArea();
 				JButton bT = new JButton("Sign");
-				
+
 				tA.setText(c.toString());
 				tA.setEditable(false);
-				panel.add(tA, BorderLayout.WEST);
+				panel.add(tA, BorderLayout.CENTER);
 				panel.add(bT, BorderLayout.EAST);
-
-				bT.addMouseListener(new MouseClickListener() {
-					@Override
-					public void mouseClicked(MouseEvent e) {
-						c.secondPartySign(true);
-						if (c.isSigned()) {
-							c.signContract();
-							Utils.CONTRACT_SIGNED.show();
-						} else
-							Utils.OTHER_PARTY_PENDING.show();
-					}
-				});
 			}
-			comp.add(panel);
+			this.add(panel);
+			return this;
 		}
-		
-		JPanel midPanel = new ListPanel(comp);
-        main.add(midPanel);
-		JScrollPane scrollp = new JScrollPane(midPanel);
-		main.add(scrollp);
-		this.display.setVisible();
 
 	}
 
+
+	@Override
+	public void update() {
+
+	}
 }
