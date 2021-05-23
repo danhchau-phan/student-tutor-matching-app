a76aede34692b940e4af0c5ad550eda426b1eb32
Yuan Vei Shen <vyua0001@student.monash.edu>
2021-05-23 19:42:33 +0800

    Tutor Monitor and Monitor in progress

--- Assignment - copy/src/tutorview/TutorMonitorView.java
+++ Assignment - copy/src/tutorview/TutorMonitorView.java
@@ -2,6 +2,7 @@ package tutorview;
 
 import mainview.Observer;
 import model.Bid;
+import model.BidResponse;
 import model.EventType;
 import model.Monitor;
 
@@ -9,6 +10,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 
@@ -18,6 +21,11 @@ public class TutorMonitorView extends JPanel {
 //    private static final int threadSleep = 1000000;
 //    private static final int monitorIntervalCheck = 5000;
 //    private Monitor monitor;
+    private List<Bid> activeBidList;
+    private List<JPanel> bidPanels = new ArrayList<>();
+    private JList<BidResponse> responseList;
+    private List<BidResponse> responses;
+
 
     public TutorMonitorView() {
         super(new BorderLayout());
@@ -31,7 +39,60 @@ public class TutorMonitorView extends JPanel {
 //    }
 
     protected void placeComponents(){
+//        this.removeAll();
 
+
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setViewportBorder(null);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        JPanel AllBidContainer = new JPanel(new GridLayout(0, 1));
+
+
+        // Create a Bid Panel for each bid
+        for (Bid activeBid: activeBidList) {
+            JPanel activeBidPanel = new JPanel();
+
+            // Responses JList
+            DefaultListModel<BidResponse> model = new DefaultListModel<>();
+            responses = activeBid.getResponse();
+            for (BidResponse r : responses)
+                model.addElement(r);
+            responseList = new JList<>(model);
+            responseList.setCellRenderer(new ResponseCellRenderer());
+
+
+            activeBidPanel.add(new JLabel(activeBid.getId(), JLabel.LEFT));
+            activeBidPanel.add(responseList);
+
+            AllBidContainer.add(activeBidPanel);
+
+        }
+
+        scrollPane.setViewportView(AllBidContainer);
+
+        setLayout(new BorderLayout());
+        add(scrollPane, BorderLayout.CENTER);
+    }
+
+    private class ResponseCellRenderer extends JPanel implements ListCellRenderer<BidResponse> {
+
+        @Override
+        public Component getListCellRendererComponent(JList<? extends BidResponse> list, BidResponse value, int index,
+                                                      boolean isSelected, boolean cellHasFocus) {
+            this.removeAll();
+
+            JPanel panel = new JPanel(new BorderLayout());
+            JEditorPane eP = new JEditorPane();
+            JButton bT = new JButton("Select Bid");
+
+            eP.setText(value.toString());
+            panel.add(eP);
+            panel.add(bT, BorderLayout.EAST);
+
+            this.add(panel);
+            return this;
+        }
     }
 
     public void add() {
@@ -42,6 +103,7 @@ public class TutorMonitorView extends JPanel {
 
     }
 
+    /** Update the latest Bid Response or expired Bid Request*/
     public void setLatestMonitorView(Set<Bid> bids) {
 
     }
