584d8429f1717b959383688b3a15ad242d7099f4
npha0029 <npha0029@student.monash.edu>
2021-05-24 08:35:32 +0700

    add nearexpirycontractframe

--- Assignment - copy/src/mainview/NearExpiryContractFrame.java
+++ Assignment - copy/src/mainview/NearExpiryContractFrame.java
@@ -1,7 +1,49 @@
 package mainview;
 
+import java.awt.Component;
+import java.util.List;
+
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ListCellRenderer;
+
+import model.Contract;
 
 public class NearExpiryContractFrame extends JFrame{
-    
+    List<Contract> contracts;
+    public NearExpiryContractFrame(List<Contract> contracts) {
+        this.contracts = contracts;
+    }
+
+    public void show() {
+        JPanel panel = new JPanel();
+        JList<Contract> cList;
+        DefaultListModel<Contract> model = new DefaultListModel<>();
+        for (Contract c : contracts) {
+            model.addElement(c);
+        }
+        cList = new JList<>(model);
+        cList.setCellRenderer(new ListCellRenderer<Contract>(){
+
+            @Override
+            public Component getListCellRendererComponent(JList<? extends Contract> list, Contract contract, int index,
+                    boolean isSelected, boolean cellHasFocus) {
+                JTextArea tA = new JTextArea();
+                tA.setText(contract.toString());
+                tA.setEditable(false);
+                return tA; // DOUBLE CHECK
+            }
+            
+        });
+
+        JScrollPane scrollp = new JScrollPane(cList);
+		panel.add(scrollp);
+
+        JOptionPane.showMessageDialog(this, panel, "Contracts About To Expire", JOptionPane.INFORMATION_MESSAGE);
+    }
 }
