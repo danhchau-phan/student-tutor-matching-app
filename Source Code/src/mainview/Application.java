dce4643d7c9b651585898a4bd5a121e2657a8bc1
npha0029 <npha0029@student.monash.edu>
2021-05-28 22:24:40 +0700

    add design rationale

--- Source Code/src/mainview/Application.java
+++ Source Code/src/mainview/Application.java
@@ -7,9 +7,9 @@ import java.util.Scanner;
 import model.Contract;
 
 public class Application {
-	   public static final String API_KEY_FILE_NAME = "/Users/yuanleslie/Desktop/Final_FIT3077/Source Code/src/apiKey"; // change this to your file's name
+//	   public static final String API_KEY_FILE_NAME = "/Users/yuanleslie/Desktop/Final_FIT3077/Source Code/src/apiKey"; // change this to your file's name
 	// public static final String API_KEY_FILE_NAME = "Source Code/apiKey"; // change this to your file's name
-//	public static final String API_KEY_FILE_NAME = "C:/Users/chaup/project/Source Code/src/apiKey"; // change this to your file's name
+	public static final String API_KEY_FILE_NAME = "C:/Users/chaup/project/Source Code/src/apiKey"; // change this to your file's name
 	public static String myApiKey;
 	public static final String rootUrl = "https://fit3077.com/api/v2";
     
